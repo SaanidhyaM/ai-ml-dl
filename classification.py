de822f49c10628d665d58a4bf603eb6c0a4756e5
Saanidhya Mittal <f20220924@pilani.bits-pilani.ac.in>
2024-01-20 15:49:19 +0530

    added feature columns

--- classification.py
+++ classification.py
@@ -30,3 +30,8 @@ def input_fn(features, labels, training=True, batch_size=256):
         dataset = dataset.shuffle(1000).repeat()
     
     return dataset.batch(batch_size)
+
+my_feature_columns = []
+for key in train.keys():
+    my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+print(my_feature_columns)
