a8315104e17e9a1494f8b98b822992af53f9221e
Saanidhya Mittal <f20220924@pilani.bits-pilani.ac.in>
2024-01-22 11:58:32 +0530

    changed all files

--- classification.py
+++ classification.py
@@ -7,7 +7,6 @@ import pandas as pd
 
 CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
 SPECIES = ['Setosa', 'Versicolor', 'Virginica']
-# Lets define some constants to help us later on
 
 train_path = tf.keras.utils.get_file(
     "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
@@ -16,16 +15,12 @@ test_path = tf.keras.utils.get_file(
 
 train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
 test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
-# Here we use keras (a module inside of TensorFlow) to grab our datasets and read them into a pandas dataframe
 
 train_y = train.pop('Species')
 test_y = test.pop('Species')
 
 def input_fn(features, labels, training=True, batch_size=256):
-    # Convert the inputs to a Dataset.
     dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
-
-    # Shuffle and repeat if you are in training mode.
     if training:
         dataset = dataset.shuffle(1000).repeat()
     
@@ -36,18 +31,14 @@ for key in train.keys():
     my_feature_columns.append(tf.feature_column.numeric_column(key=key))
 print(my_feature_columns)
 
-# Build a DNN with 2 hidden layers with 30 and 10 hidden nodes each.
 classifier = tf.estimator.DNNClassifier(
     feature_columns=my_feature_columns,
-    # Two hidden layers of 30 and 10 nodes respectively.
     hidden_units=[30, 10],
-    # The model must choose between 3 classes.
     n_classes=3)
 
 classifier.train(
     input_fn=lambda: input_fn(train, train_y, training=True),
     steps=5000)
-# We include a lambda to avoid creating an inner function previously
 
 eval_result = classifier.evaluate(
     input_fn=lambda: input_fn(test, test_y, training=False))
@@ -55,7 +46,6 @@ eval_result = classifier.evaluate(
 print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
 
 def input_fn(features, batch_size=256):
-    # Convert the inputs to a Dataset without labels.
     return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
 
 features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
--- linear regression.py
+++ linear regression.py
@@ -20,29 +20,28 @@ NUMERIC_COLUMNS = ['age', 'fare']
 
 feature_columns = []
 for feature_name in CATEGORICAL_COLUMNS:
-  vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
+  vocabulary = dftrain[feature_name].unique()
   feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
 
 for feature_name in NUMERIC_COLUMNS:
   feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
 def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
-  def input_function():  # inner function, this will be returned
-    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+  def input_function():
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df)) 
     if shuffle:
-      ds = ds.shuffle(1000)  # randomize order of data
-    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
-    return ds  # return a batch of the dataset
-  return input_function  # return a function object for use
+      ds = ds.shuffle(1000)
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    return ds
+  return input_function
 
-train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+train_input_fn = make_input_fn(dftrain, y_train)
 eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
 
 linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
-# We create a linear estimtor by passing the feature columns we created earlier
 
-linear_est.train(train_input_fn)  # train
-result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
+linear_est.train(train_input_fn)
+result = linear_est.evaluate(eval_input_fn)
 
-clear_output()  # clears console output
-print(result['accuracy'])  # the result variable is simply a dict of stats about our model
+clear_output()
+print(result['accuracy'])
