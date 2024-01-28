a733b3ac66ac1fbad6fbe5359c9be5ceb94b45d6
Saanidhya Mittal <f20220924@pilani.bits-pilani.ac.in>
2024-01-28 15:32:36 +0530

    updated fashion prog

--- fashion.py
+++ fashion.py
@@ -1,14 +1,12 @@
-# TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
 
-# Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
-fashion_mnist = keras.datasets.fashion_mnist  # load dataset
+fashion_mnist = keras.datasets.fashion_mnist 
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  # split into tetsing and training
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
@@ -18,9 +16,9 @@ train_images = train_images / 255.0
 test_images = test_images / 255.0
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1)
-    keras.layers.Dense(128, activation='relu'),  # hidden layer (2)
-    keras.layers.Dense(10, activation='softmax') # output layer (3)
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
 ])
 
 model.compile(optimizer='adam',
